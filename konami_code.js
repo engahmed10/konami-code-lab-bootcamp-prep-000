fbfb8d22b26a0a7fdcfa75888a33cb1bc6622d05
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-17 16:41:56 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,7 @@ const codes = [
 function init() 
 {
   // your code here
-   document.body.addEventListener("keydown", (event) => {
+     document.body.addEventListener("keydown", (event) => {
      KeyDownP(e);
   })
 }
